d361f07da58ed84307954dcad2ca1bbb626d35ee
johnson1204 <34064271+johnson1204@users.noreply.github.com>
2019-03-17 12:03:53 +0800

    The scripts used in the small game

---
+++ zijijv.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zijijv : MonoBehaviour
+{
+    private Transform target;
+    public float moveSpeed = 3f;
+    private Vector3 direction;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        GameObject go = GameObject.FindGameObjectWithTag("player");
+        target = go.transform;
+        direction = (target.position - transform.position).normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.DrawLine(target.transform.position, this.transform.position, Color.yellow);
+
+        //lock at target Player       
+        //Move towards target
+
+        transform.position += direction * moveSpeed * Time.deltaTime;
+    }
+}
